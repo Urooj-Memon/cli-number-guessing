a773196268f5ee3d4e1fccf957459ebf10cc3f2b
Urooj-Memon <164159799+Urooj-Memon@users.noreply.github.com>
2024-03-24 02:52:50 +0500

    created number guessing game
    created number guessing game

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const randomNamber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessednumber",
+        type: "number",
+        message: "Please guess a number between 1-6:",
+    },
+]) 
+if (answers.userGuessednumber === randomNamber) {
+    console.log("Congratulations! you guessed right number.");
+}
+else {
+    console.log("You guessed wrong number");
+};
